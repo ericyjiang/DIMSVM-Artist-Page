24496f17ca82f717e0c79895086074e459f3edba
Eric Jiang <eyjiang18@gmail.com>
2022-09-15 14:03:02 -0400

    added assets and styling

--- pages/posts/upcoming.js
+++ pages/posts/upcoming.js
@@ -1,13 +1,17 @@
+import Head from "next/head";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title> Upcoming Shows</title>
+      </Head>
       <h1>Upcoming Shows</h1>
       <h2>
-        {" "}
         <Link href="/"> Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
