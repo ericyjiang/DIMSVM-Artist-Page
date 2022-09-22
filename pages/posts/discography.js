05ec0e995db7eb7601093d2946960eda88ac192b
dvebino <dhuang2495@gmail.com>
2022-09-22 17:42:39 -0400

    soundcloud link added, need iFrame Spotify

--- pages/posts/discography.js
+++ pages/posts/discography.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Layout from "../../components/layout";
 
 export default function Discography() {
+
   return (
     <Layout>
       <Head>
@@ -12,9 +13,18 @@ export default function Discography() {
       <div>
         <a href="https://soundcloud.com/dimsvm">SoundCloud</a>
       </div>
+      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/06CnAdKDLvYsj5A8x8BjF7?utm_source=generator&theme=0" 
+      width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+      loading="lazy">
+      </iframe>
       <h2>
         <Link href="/"> Back to home </Link>
       </h2>
     </Layout>
   );
 }
+
+/* <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/06CnAdKDLvYsj5A8x8BjF7?utm_source=generator&theme=0" 
+width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+loading="lazy"></iframe>
+*/
