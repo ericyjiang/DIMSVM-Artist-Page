687d60ebc0faadc634abe7032509c4251f541e93
Eric Jiang <eyjiang18@gmail.com>
2022-09-30 17:09:53 -0400

    added content and images to about page

--- pages/posts/about.js
+++ pages/posts/about.js
@@ -1,21 +1,91 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "../../components/layout";
+import utilStyles from "../../styles/utils.module.css";
+import Image from "next/image";
 
 export default function About() {
   return (
     <Layout>
       <Head>
         <title> About Us </title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;800&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <h1>About Us</h1>
       <h2>
         <Link href="/"> Back to home </Link>
       </h2>
 
+      <div className={utilStyles.eventName}>
+        <h3>About Us</h3>
+      </div>
+
+      <div>
+        <Image
+          priority
+          src="../images/duodj.png"
+          height={197}
+          width={390}
+          unoptimized={true}
+        />
+      </div>
+
+      <p>A couple friends, a couple tunes.</p>
       <p>
-        DIMSVM is a dubstep duo from NYC, consisting of Alfonso and Eric.
-        Guaranteed to bring you steamy tracks and preformances!{" "}
+        Before DIMSVM was formed, Eric and Alfonso were solo acts trying to
+        break into the local EDM scene. After playing a back-to-back set
+        together, they realized their similarities in music taste and curating
+        sets.
+      </p>
+
+      <div>
+        <Image
+          priority
+          src="../images/eezusdj.png"
+          height={312}
+          width={390}
+          unoptimized={true}
+        />
+      </div>
+
+      <p>
+        Eric is a self-taught DJ, starting his own solo project 'Eezus' in 2013.
+        His main influences consist of Skrillex, Jauz, and Subtronics. Through a
+        wide range of genres, from House, Trap, and Dubstep. Eezus never fails
+        to bring the energy to his sets.
+      </p>
+
+      <div>
+        <Image
+          priority
+          src="../images/xansdj.png"
+          height={390}
+          width={390}
+          unoptimized={true}
+        />
+      </div>
+
+      <p>
+        Alfonso originally listened to Heavy Metal. With a history of playing
+        guitar for a few local bands, the explosion of Dubstep spakred the
+        interested in producting heavy music digitally. After exploring multiple
+        styles and aliases since 2011, he discovered his love for sound design
+        and mixing multiple genres into one. His main influences consist of
+        Virtual Riot, Zomboy, and Excision.
+      </p>
+
+      <p>
+        The name DIMSVM is an ode to their Asian background and their love for
+        food. Combined DIMSVM is also the result of their shared taste and
+        attitutde towards music.
       </p>
     </Layout>
   );
