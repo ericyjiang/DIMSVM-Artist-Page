508da43e53c1ddd2dbba247e2d031e12a866fb96
Eric Jiang <eyjiang18@gmail.com>
2022-09-30 19:22:47 -0400

    added boxes to events

--- pages/posts/events.js
+++ pages/posts/events.js
@@ -1,43 +1,74 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "../../components/layout";
+import Image from "next/image";
+import utilStyles from "../../styles/utils.module.css";
 
 export default function Events() {
   return (
     <Layout>
       <Head>
         <title> Upcoming Shows</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;800&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-      <h1>Upcoming Shows</h1>
       <h2>
         <Link href="/"> Back to home </Link>
       </h2>
 
-      <p>
-        Oct. 29, 2022 <br />
-        <b>Gossip 8 </b> <br />
-        Northern Blvd, NY
-      </p>
+      <h2>Next Event</h2>
 
-      <h2> Past Shows </h2>
-      <p>
-        July 03, 2020 <br />
-        <b> Vibestream</b> <br />
-        Virtual Live Stream
-      </p>
+      <div className={utilStyles.upcomingEvent}>
+        <Image
+          priority
+          src="/images/gossip8.png"
+          height={232}
+          width={358}
+          unoptimized={true}
+        />
+      </div>
 
-      <p>
-        Feb. 15, 2020 <br />
-        <b>Topsaa </b> <br />
-        College Point, NY
-      </p>
+      <div className={utilStyles.upcomingEventDescription}>
+        <a className={utilStyles.eventPageName}>Gossip 8 Inc</a>
+        <p>
+          Oct. 20, 2022 | 10:00 pm <br />
+          153-14 Northern Blvd, Flushing NY 11354
+        </p>
+      </div>
 
-      <p>
-        June 28, 2019 <br />
-        <b> Meet </b>
-        <br />
-        Flushing, NY
-      </p>
+      <h2> Past Events</h2>
+
+      <div className={utilStyles.pastEvent}>
+        <a className={utilStyles.eventPageName}>Vibestream</a>
+        <p>
+          Jul. 03, 2020 <br />
+          Virtual Livestream Event
+        </p>
+      </div>
+
+      <div className={utilStyles.pastEvent}>
+        <a className={utilStyles.eventPageName}>Topsaa</a>
+        <p>
+          Feb. 15, 2020 <br />
+          41-14 162nd St, Queens, NY 11358
+        </p>
+      </div>
+
+      <div className={utilStyles.pastEvent}>
+        <a className={utilStyles.eventPageName}>BASSMENT</a>
+        <p>
+          Jun. 28, 2019 <br />
+          137-74 Northern Blvd, Flushing, NY 11354
+        </p>
+      </div>
     </Layout>
   );
 }
